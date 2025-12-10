bbe3661d85b129002576aa25ad0b50ab8a53fa06
Chris Opon-Nimoh <112328937+ChrisO72@users.noreply.github.com>
2025-12-10 10:57:52 +0100

    prettier and eslint

---
+++ eslint.config.ts
@@ -0,0 +1,40 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: [".react-router/"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      parser: undefined,
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
