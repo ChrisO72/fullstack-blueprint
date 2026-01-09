61f25f5c0cea6749899bc7c8392ac0b940c3888e
Chris Opon-Nimoh <112328937+ChrisO72@users.noreply.github.com>
2026-01-09 23:19:38 +0100

    gracefull shutdown for worker

--- worker/index.ts
+++ worker/index.ts
@@ -3,8 +3,18 @@ import { redisConnection } from "./redis";
 import { processJob } from "./jobs";
 import { startScheduler } from "./scheduler";
 
-new Worker("default", processJob, { connection: redisConnection });
+const worker = new Worker("default", processJob, { connection: redisConnection });
 
 startScheduler();
 
 console.log("[Worker] Ready");
+
+async function shutdown(signal: string) {
+  console.log(`[Worker] ${signal} received, shutting down...`);
+  await worker.close();
+  console.log("[Worker] Shutdown complete");
+  process.exit(0);
+}
+
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+process.on("SIGINT", () => shutdown("SIGINT"));
