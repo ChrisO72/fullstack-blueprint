58f8e86f537187357ebc7a59688e3f3d55cea27c
Chris Opon-Nimoh <112328937+ChrisO72@users.noreply.github.com>
2026-04-19 22:52:02 +0200

    fix eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    ignores: [".react-router/"],
+    ignores: [".react-router/", "web/components/ui-kit/"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
